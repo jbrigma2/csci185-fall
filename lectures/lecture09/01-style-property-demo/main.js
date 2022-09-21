ef3865f0a29280a9e8aacf138d0040bc35f3ecbf
Jada <jbrigma2@unca.edu>
2022-09-20 21:36:45 -0400

    Tutorial 5

---
+++ lectures/lecture09/01-style-property-demo/main.js
@@ -0,0 +1,24 @@
+
+const makeRed = () => {
+    // your code here...
+    console.log('Change background to red');
+    document.querySelector('body').style.backgroundColor = 'red';
+    document.querySelector( '#section1').style.backgroundColor= 'green';
+    document.querySelector
+};
+
+const makeBlue = () => {
+    // your code here...
+    console.log('Change background to blue');
+};
+
+const makePink = () => {
+    // your code here...
+    console.log('Change background to pink');
+};
+
+const makeOrange = () => {
+    // your code here...
+    console.log('Change background to orange');
+};
+
---
+++ lectures/lecture09/03-all-of-the-above/script.js
@@ -0,0 +1,20 @@
+const changeColor = () => {
+    document.querySelector('.panel').style.background = 'hotpink';
+};
+
+const changeTitle = () => {
+    //what do we want to change?
+    document.querySelector('h1').innerText= "Some Message";
+};
+
+const addImage = () => {
+    // adds the following image to each panel:
+    // 1. what element do you want to select?
+    //`<img src="https://media1.britannica.com/eb-media/22/65322-004-8FF21CDA.jpg" />`;
+    document.querySelector
+
+};
+
+const clearDiv = () => {
+    // clears all of the panels of content
+};
