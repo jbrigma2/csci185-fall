69b711e6cc4689de78e7aad9a89647c29cc75755
Jada <jbrigma2@unca.edu>
2022-10-10 10:39:30 -0400

    HW3

--- homework/hw03/sketch.js
+++ homework/hw03/sketch.js
@@ -18,10 +18,52 @@ function setup() {
     drawCircle(550,100,10,'white');
     drawCircle(650,100,10,'white');
 
+    //side of pants 
+    stroke('red');
+    strokeWeight(5);
+    line(550, 100, 450, 300);
 
     stroke('red');
     strokeWeight(5);
-    line(543, 100, 0, 100);
+    line(650, 100, 750, 300);
+
+    //bottom of pants 
+    strokeWeight(5);
+    stroke('black');
+    noFill();
+    beginShape();
+    vertex(450, 300);
+    vertex(550, 350);
+    vertex(600, 300);
+    vertex(650, 350);
+    vertex(750, 300);
+    endShape();
+
+    //legs
+    stroke('black');
+    strokeWeight(5);
+    line(470, 310, 320, 550);
+
+    stroke('black');
+    strokeWeight(5);
+    line(504, 330, 368, 550);
+
+    stroke('black');
+    strokeWeight(5);
+    line(696, 330, 820, 550);
+
+    stroke('black');
+    strokeWeight(5);
+    line(730, 310, 868, 550);
+
+    //feet
+    rect(295, 552, 100, 40);
+
+    rect(805, 552, 100, 40);
+
+
+
+
 
     drawGrid(canvasWidth, canvasHeight);
 }
