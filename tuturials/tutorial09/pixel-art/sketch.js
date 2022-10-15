740cabe336d5fb377f025b94b12e615fefe4f64e
Jada <jbrigma2@unca.edu>
2022-10-14 20:27:22 -0400

    HW2

--- tuturials/tutorial09/pixel-art/sketch.js
+++ tuturials/tutorial09/pixel-art/sketch.js
@@ -36,13 +36,29 @@ function setup() {
     createCanvas(canvasWidth, canvasHeight);
     drawGrid(canvasWidth, canvasHeight);
 
-    // first 6 rows of frank
-    drawRow(frank[0], 150, 150, 25);
-    drawRow(frank[1], 150, 175, 25);
-    drawRow(frank[2], 150, 200, 25);
-    drawRow(frank[3], 150, 225, 25);
-    drawRow(frank[4], 150, 250, 25);
-    drawRow(frank[5], 150, 275, 25);
+    //wrap this line in a while loop
+    //what changes?
+    //which row prints
+    //the y increaes by the pixel size
+    
+    let i=0;
+    let y = 150;
+    while (i< frank.length){
+        drawRow(frank[0], 150, 150, 25);
+        ++i;
+        y+= 25;
+
+}
+
+
+
+    i = 0;
+    y= 350;
+    while (i < heart.legth){
+        drawRow(heart[i], 150, y, 15);
+        ++1;
+        y +=15;
+    }
 
     // first 6 rows of the heart
     drawRow(heart[0], 450, 350, 15);
@@ -57,7 +73,17 @@ function setup() {
 function drawRow (row, topX, topY, pixelWidth) {
 
     for (let i = 0; i < row.length; i++) {
-        fill('#CCC');
+        //colornum gives us the color of the pixel 
+        let colorNum = row [i]; //0,1,2,3
+        if (colorNum === 0) {
+            noFill();
+        } else if (colorNum === 1) {
+            fill('pink ');
+        } else if (colorNum === 2) {
+            fill ('black');
+        } else {
+            fill('white');
+        }
         square(topX, topY, pixelWidth)
         topX += pixelWidth;
     }
