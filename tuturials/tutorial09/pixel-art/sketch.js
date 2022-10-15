496f3bd30a091d4daf31794a5e28325cfe1d1ac9
Jada <jbrigma2@unca.edu>
2022-10-14 20:48:04 -0400

    Tutorial9

--- tuturials/tutorial09/pixel-art/sketch.js
+++ tuturials/tutorial09/pixel-art/sketch.js
@@ -35,39 +35,22 @@ const frank = [
 function setup() {
     createCanvas(canvasWidth, canvasHeight);
     drawGrid(canvasWidth, canvasHeight);
-
-    //wrap this line in a while loop
-    //what changes?
-    //which row prints
-    //the y increaes by the pixel size
-    
-    let i=0;
-    let y = 150;
-    while (i< frank.length){
-        drawRow(frank[0], 150, 150, 25);
-        ++i;
-        y+= 25;
-
+drawPixelArt( heart, 20, 20,15);
+drawPixelArt(frank,120, 250,12);
+drawPixelArt(heart,420, 250,8);
+drawPixelArt(heart,55,415,6);
+drawPixelArt(heart,350,135,5);
+drawPixelArt(frank,315,380,15);
+drawPixelArt(frank,420,10,10);
 }
 
-
-
-    i = 0;
-    y= 350;
-    while (i < heart.legth){
-        drawRow(heart[i], 150, y, 15);
-        ++1;
-        y +=15;
+function drawPixelArt(grid,x,y,pixel){
+    let i =0;
+    while(i <grid.length) {
+        drawRow(grid[i],x ,y, pixel);
+        ++i;
+        y+=pixel;
     }
-
-    // first 6 rows of the heart
-    drawRow(heart[0], 450, 350, 15);
-    drawRow(heart[1], 450, 365, 15);
-    drawRow(heart[2], 450, 380, 15);
-    drawRow(heart[3], 450, 395, 15);
-    drawRow(heart[4], 450, 410, 15);
-    drawRow(heart[5], 450, 425, 15);
-
 }
 
 function drawRow (row, topX, topY, pixelWidth) {
